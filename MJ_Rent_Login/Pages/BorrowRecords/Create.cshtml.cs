e3d0b69311191e3ecf808cd577d059e8e091f37f
BboyJayJ <fbiko888@gmail.com>
2023-04-23 19:41:49 +0800

    部分介面修改

--- MJ_Rent_Login/Pages/BorrowRecords/Create.cshtml.cs
+++ MJ_Rent_Login/Pages/BorrowRecords/Create.cshtml.cs
@@ -7,12 +7,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using MJ_Rent_Login.Data;
 using MJ_Rent_Login.Models;
+using NuGet.Packaging.Signing;
 
 namespace MJ_Rent_Login.Pages.BorrowRecords
 {
     public class CreateModel : PageModel
     {
         private readonly MJ_Rent_Login.Data.ApplicationDbContext _context;
+
+        public SelectList? Id1 { get; set; }
+
+        public SelectList? UserId1 { get; set; }
 
         public CreateModel(MJ_Rent_Login.Data.ApplicationDbContext context)
         {
@@ -21,6 +26,17 @@ namespace MJ_Rent_Login.Pages.BorrowRecords
 
         public IActionResult OnGet()
         {
+            var query = from room in _context.MeetRoom
+                        select new { room.Id, room.Name };
+
+
+            Id1 = new SelectList(query, "Id", "Name");
+
+            var UserId_query1 = from user in _context.Reserve
+                               select user.UserId;
+
+            UserId1 = new SelectList(UserId_query1, "UserId");
+
             return Page();
         }
 
